48f548a8f0a4820b991abc09352a20b21b06e601
Ian <ianvzs@outlook.com>
2022-04-06 23:03:00 +0800

    add virtual joystick

--- Main.gd
+++ Main.gd
@@ -1,9 +1,6 @@
 extends Node
 
 export (PackedScene) var mob_scene
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
 
 # Called when the node enters the scene tree for the first time.
--- Player.gd
+++ Player.gd
@@ -1,5 +1,8 @@
 extends KinematicBody
 
+export (NodePath) var joystickLeftPath
+onready var joystickLeft : VirtualJoystick = get_node(joystickLeftPath)
+
 export var speed = 14
 export var fall_acceleration = 75
 export var jump_impulse = 20
@@ -13,6 +16,9 @@ var velocity = Vector3.ZERO
 
 func _physics_process(delta):
 	var direction = Vector3.ZERO
+	direction.x = Input.get_axis("ui_left", "ui_right")
+	direction.z = Input.get_axis("ui_up", "ui_down")
+	
 	# We check for each move input and update the direction accordingly.
 	if Input.is_action_pressed("move_right"):
 		direction.x += 1
